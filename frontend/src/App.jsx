8a35d528a43aaf3e1826bc952bbd9afcb8a37651
Youssef-Touria <touria.youssef@laplateforme.io>
2026-01-28 15:37:01 +0100

    Ajout navbar marsAI avec animations et design responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import WpPage from "./pages/WpPage";
 import NotFound from "./pages/NotFound";
@@ -7,15 +7,12 @@ import NotFound from "./pages/NotFound";
 export default function App() {
   return (
     <>
-      <Header />
-
+      <Navbar />   
       <Routes>
         {/* Home = React */}
         <Route path="/" element={<Home />} />
-
         {/* Toutes les pages WordPress (par slug) */}
         <Route path="/:slug" element={<WpPage />} />
-
         {/* 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
