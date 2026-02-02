17a23821b6700cb71d148db0c117ada1537c12d3
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-02-02 15:31:57 +0100

    .

--- frontend/src/pages/WpPage.jsx
+++ frontend/src/pages/WpPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPageBySlug } from "../api";
 import NotFound from "./NotFound";
-//import "./contact.css";
 
 export default function WpPage({ isHome = false }) {
   const { slug: routeSlug } = useParams();
