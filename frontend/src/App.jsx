4c21e1a861b3efb09a2af03b7cc0c37d82e851bf
Youssef-Touria <touria.youssef@laplateforme.io>
2026-02-03 14:51:44 +0100

    commit all new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-=======
-import Navbar from './components/Navbar';
->>>>>>> e166919c47d5d9fdd0437d426572526463f29fac
 import Home from "./pages/Home";
 import WpPage from "./pages/WpPage";
 import NotFound from "./pages/NotFound";
@@ -17,11 +12,10 @@ export default function App() {
       <Routes>
         {/* Home = React */}
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
+
         <Route path="/cgv" element={<CGV />} />
         <Route path="/cgu" element={<CGU />} />
-=======
->>>>>>> e166919c47d5d9fdd0437d426572526463f29fac
+
         {/* Toutes les pages WordPress (par slug) */}
         <Route path="/:slug" element={<WpPage />} />
         {/* 404 */}
