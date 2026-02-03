751743819997854a8c1e25a7dc3f6f905d6214a2
Youssef-Touria <touria.youssef@laplateforme.io>
2026-02-03 10:14:11 +0100

    Ajout des pages CGV et CGU

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,25 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import WpPage from "./pages/WpPage";
 import NotFound from "./pages/NotFound";
+import CGV from './pages/CGV';
+import CGU from './pages/CGU';
 
 export default function App() {
   return (
     <>
       <Header />
-
       <Routes>
         {/* Home = React */}
         <Route path="/" element={<Home />} />
-
+        
+        {/* Pages statiques  */}
+        <Route path="/cgv" element={<CGV />} />
+        <Route path="/cgu" element={<CGU />} />
+        
         {/* Toutes les pages WordPress (par slug) */}
         <Route path="/:slug" element={<WpPage />} />
-
-        {/* 404 */}
+        
+        {/* 404  */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
