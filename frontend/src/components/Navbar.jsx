67f8dd5e96c5fc72668aa87c09ca4ae6614d9bed
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-02-10 09:30:51 +0100

    edit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-20">
             {/* Logo (Inchangé) */}
-            <Link to="/" className="flex items-center space-x-3 group">
+            <Link to="/accueil" className="flex items-center space-x-3 group">
               <div className="relative">
                 <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-cyan-400 to-pink-500 flex items-center justify-center transform group-hover:scale-110 transition-transform duration-300">
                   <svg
