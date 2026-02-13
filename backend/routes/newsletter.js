bb9fe4a120eb65fbc24cbd94af6cf3419f34f628
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-02-13 08:59:11 +0100

    .

--- backend/routes/newsletter.js
+++ backend/routes/newsletter.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const router = express.Router(); // <--- Utilise bien le Router d'Express
+const router = express.Router();
 const newsletterController = require("../controllers/newsletter");
 
-// Ligne 5 (celle qui posait problème)
 router.post("/subscribe", newsletterController.subscribe);
 
 module.exports = router;
