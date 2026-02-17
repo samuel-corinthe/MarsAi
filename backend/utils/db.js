5d05c7c298e78f605dcb242325f3ea54e61dd89a
Jossnnier <fauconnierjosselin@yahoo.fr>
2026-02-17 10:37:01 +0100

    petit chanchement

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 20,
 });
 
 export default pool;
